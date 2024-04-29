0a6877f4ef6928326ab997020f388bb48ae58298
Shivam Agrawal <118833469+scodemuse@users.noreply.github.com>
2024-04-29 09:05:23 +0000

    conversion

---
+++ bsics/conversion_operation.js
@@ -0,0 +1,20 @@
+let score="33"
+
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber);
+
+
+// "33"=>=33
+// "33abc"=> NaN
+
+let isLogedIn=1
+let booleanIsLoggedIn=Boolean(isLogedIn)
+console.log(booleanIsLoggedIn);
+
+//1=> true; 0=> false;
+//""=>false
+// "hello"=>true
+
